1cb1b9604c32095b40e52c74b97f001480810481
anastasiarus <rusak200038@gmail.com>
2022-01-28 12:50:31 +0300

    messages

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import classes from './Messages.module.css'
-//import {NavLink } from "react-router-dom";
+import axios from "axios"
 
 
 const Messages = () =>{
+
+  const [friends, setFriends] = useState([]);
+
+  const getFriends = async () => {
+    const { data } = await axios({
+      url: "http://localhost:5000/api/friends",
+      method: "GET",
+    });
+    setFriends(data);
+  };
+  useEffect(() => {
+    getFriends();
+  }, []);
+
     return (
     <div className={classes.main}>
-       <p>Сообщения</p>
+       <div className={classes.dialogs}>
+            <div className={classes.item} >
+           { friends.map(friend => (
+              <li className={classes.friends} key={friend._id}>{friend.firstName} {friend.lastName} </li>
+           ))}
+            </div>
+       </div>
+       <div className={classes.messages}>
+        <div className={classes.message}>
+            Привет
+        </div>
+        <div className={classes.message}>
+            Как дела?
+        </div>
+       </div>
     </div>
     )
 }
